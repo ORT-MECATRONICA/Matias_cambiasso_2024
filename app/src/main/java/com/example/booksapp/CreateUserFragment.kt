b9c76fd4237c6d123cf9ee57e33caa3309e72470
isabellamastrangelo <china.mastrangelo@gmail.com>
2023-06-26 21:46:52 -0300

    hay interfaz de log in - falta implementar viewmodel

--- app/src/main/java/com/example/booksapp/CreateUserFragment.kt
+++ app/src/main/java/com/example/booksapp/CreateUserFragment.kt
@@ -6,6 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.navigation.findNavController
+import com.google.android.material.snackbar.Snackbar
 
 class CreateUserFragment : Fragment() {
 
@@ -14,18 +18,40 @@ class CreateUserFragment : Fragment() {
     }
 
     private lateinit var viewModel: CreateUserViewModel
-
+    private lateinit var v : View
+    private lateinit var username: EditText
+    private lateinit var password: EditText
+    private lateinit var createButton: Button
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_create_user, container, false)
+        v = inflater.inflate(R.layout.fragment_create_user, container, false)
+        username = v.findViewById(R.id.createUsername)
+        password = v.findViewById(R.id.createPassword)
+        createButton = v.findViewById(R.id.createButton)
+        return v;
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(CreateUserViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        createButton.setOnClickListener {
+            val myUser: String = username.text.toString()
+            val myPassword: String = password.text.toString()
+
+            if (myUser.isEmpty() || myPassword.isEmpty()) {
+                Snackbar.make(v, "Please insert your Username and Password", Snackbar.LENGTH_SHORT)
+                    .show()
+            }
+            else {
+                //HAY QUE VER COMO HACER PARA SUMAR ESTO A LA LISTA DE USUARIOS
+                Snackbar.make(v, "Insert username", Snackbar.LENGTH_SHORT).show()
+                view?.findNavController()?.navigate(R.id.action_createUserFragment_to_bookListFragment)
+            }
+        }
     }
+
 
 }
